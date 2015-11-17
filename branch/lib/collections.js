8f1c97ddf754eaf577b2b727b506e1d8039942cd
xavior1777 <xavvior@gmail.com>
2015-11-17 22:41:09 +0100

    Database struct

--- branch/lib/collections.js
+++ branch/lib/collections.js
@@ -2,4 +2,15 @@
  * Creates the Messages MongoDB collection.
  * @type {Mongo.Collection}
  */
-Messages = new Mongo.Collection('messages');
+ /*
+	megnevezes,idopont,eredmeny,sport_id,ki1,ki2
+ */
+esemeny = new Mongo.Collection('esemeny');
+/*
+	id,megnevezes
+*/
+sportok = new Mongo.Collection('sportok');
+/*
+	username,password,szint
+*/
+felhasznalo = new Mongo.Collection('felhasznalo');
